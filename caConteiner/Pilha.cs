cc95072ca37b913b43deeb50ec0780cc87b31852
duh-h <cadugon1000@yahoo.com>
2023-09-19 17:14:46 -0300

    Adicionar arquivos de projeto.

---
+++ caConteiner/Pilha.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caConteiner
+{
+    internal class Pilha
+    {
+        public Noh Head { get; set; }
+        public int Contador;
+
+
+        public Pilha()
+        {
+            this.Head = new Noh();
+            this.Head.anterior = null;
+            this.Head.proximo = null;
+            this.Contador = 0;
+        }
+
+        public void addFim(int info)
+        {
+            if (Contador < 3)
+            {
+                Noh atual = Head.proximo;
+
+                while (atual != null)
+                {
+                    if (atual.info == info)
+                    {
+                        Console.WriteLine("O Codigo já existe na Pilha.");
+                        return;
+                    }
+                    atual = atual.proximo;
+                }
+
+                Noh novoNoh = new Noh(info);
+
+                if (this.Head.anterior == null)
+                {
+                    this.Head.anterior = novoNoh;
+                    this.Head.proximo = novoNoh;
+                }
+                else
+                {
+                    novoNoh.anterior = this.Head.proximo;
+                    this.Head.proximo.proximo = novoNoh;
+                    this.Head.proximo = novoNoh;
+                }
+
+                this.Contador++;
+            }
+            else
+            {
+                Console.WriteLine("Nao e possivel adicionar mais containers na pilha");
+            }
+        }
+
+
+        public void Imprimirlista()
+        {
+            Noh atual = Head.anterior;
+
+            while (atual != null)
+            {
+                Console.WriteLine(atual.info);
+                atual = atual.proximo;
+            }
+
+            Console.WriteLine();
+        }
+        public int RemoveFim(int info)
+        {
+            if (Head.proximo == null)
+            {
+                Console.WriteLine("A Pilha está vazia.");
+                return -1;
+            }
+            else
+            {
+                if (info == Head.proximo.info)
+                {
+
+                    int removido = Head.proximo.info;
+
+                    if (Head.anterior == Head.proximo)
+                    {
+                        Head.anterior = null; ; Head.proximo = null;
+                    }
+                    else
+                    {
+                        Head.proximo = Head.proximo.anterior;
+                        Head.proximo.proximo = null;
+                    }
+                    this.Contador--;
+
+                    return removido;
+                }
+                Console.WriteLine("Codigo Invalido");
+                return -1;
+            }
+        }
+    }
+}
---
+++ caConteiner/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caConteiner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilha pilha1 = new Pilha();
+            Pilha pilha2 = new Pilha();
+            Pilha pilha3 = new Pilha();
+            Pilha pilha4 = new Pilha();
+            int codigo;
+
+            Console.WriteLine("[1] Inserir contenier");
+            Console.WriteLine("[2] Remover contanier");
+            Console.WriteLine("[3] SAIR");
+
+            string controle;
+            string lugaradd, lugarRemove;
+            controle=Console.ReadLine();
+            while(controle != "3")
+            {
+                switch (controle)
+                {
+                    case "1":
+                        Console.WriteLine("Adicionar na pilha 1,2,3 ou 4?");
+                        lugaradd =Console.ReadLine();
+                        switch (lugaradd)
+                        {
+                            case "1":
+                                Console.WriteLine("Digite o codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha1.addFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha1.Imprimirlista();
+
+                                break;
+
+
+
+                            case "2":
+                                Console.WriteLine("Digite o codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha2.addFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha3.Imprimirlista();
+                                break;
+
+
+                            case "3":
+                                Console.WriteLine("Digite o codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha3.addFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha3.Imprimirlista();
+                                break;
+
+
+
+                            case "4":
+                                Console.WriteLine("Digite o codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha4.addFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha4.Imprimirlista();
+                                break;
+
+
+
+                            default:
+                                Console.WriteLine("Opção inválida. Tente novamente.");
+                                break;
+
+
+
+
+                        }
+                        break;
+
+
+
+
+                    case "2":
+                        Console.WriteLine("remover na pilha 1,2,3 ou 4?");
+                        lugarRemove = Console.ReadLine();
+                        switch (lugarRemove)
+                        {
+                            case "1":
+                                Console.WriteLine("Digite o Codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha1.RemoveFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha1.Imprimirlista();
+                                break;
+
+
+
+                            case "2":
+                                Console.WriteLine("Digite o Codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha2.RemoveFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha2.Imprimirlista();
+                                break;
+
+
+                            case "3":
+                                Console.WriteLine("Digite o Codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha3.RemoveFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha3.Imprimirlista();
+                                break;
+
+
+
+                            case "4":
+                                Console.WriteLine("Digite o Codigo:");
+                                codigo = Convert.ToInt32(Console.ReadLine());
+                                pilha4.RemoveFim(codigo);
+                                Console.WriteLine("Pilha:");
+                                pilha4.Imprimirlista();
+                                break;
+
+
+
+                            default:
+                                Console.WriteLine("Opção inválida. Tente novamente.");
+                                break;
+
+
+
+
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+
+
+                }
+                Console.WriteLine("[1] Inserir contenier");
+                Console.WriteLine("[2] Remover contanier");
+                Console.WriteLine("[3] SAIR");
+
+                controle = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
